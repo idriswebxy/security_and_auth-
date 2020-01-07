bf86c1b8f18d49395c80d1a3cdfa8bfba10d887e
Idris <icoder112@gmail.com>
2020-01-06 22:12:16 -0500

    server updates

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+// const md5 = require("md5");
 // const encrypt = require("mongoose-encryption");
 
+const saltRounds = 10;
 
 const app = express();
-
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -19,9 +20,9 @@ app.use(
   })
 );
 
-
-// Database 
-mongoose.connect(process.env.MONGODB,
+// Database
+mongoose.connect(
+  process.env.MONGODB,
   {
     useUnifiedTopology: true,
     useNewUrlParser: true
@@ -29,61 +30,51 @@ mongoose.connect(process.env.MONGODB,
   console.log("MongoDB Connected❗️")
 );
 
-
-// Schema 
+// Schema
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-
-
 // userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-
 
 // Model
 const User = new mongoose.model("User", userSchema);
-
 
 // Routes
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-
 app.get("/login", (req, res) => {
   res.render("login");
 });
-
 
 app.get("/register", (req, res) => {
   res.render("register");
 });
 
-
 app.get("/logout", (req, res) => {
-  res.render("home")
+  res.render("home");
 });
-
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-
-  newUser.save(err => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(err => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
-
 app.post("/login", (req, res) => {
-
   const username = req.body.username;
   const password = md5(req.body.password);
 
@@ -96,10 +87,9 @@ app.post("/login", (req, res) => {
           res.render("secrets");
         }
       }
-    } 
+    }
   });
 });
-
 
 // Port
 app.listen(process.env.PORT, () => {
