56156da1871a3afd7be92727e63eebb6707ce30f
Idris <icoder112@gmail.com>
2020-01-06 20:15:12 -0500

    server updates

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+// const encrypt = require("mongoose-encryption");
 
 
 const app = express();
@@ -37,11 +38,12 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 
 // Model
 const User = new mongoose.model("User", userSchema);
+
 
 // Routes
 app.get("/", (req, res) => {
@@ -67,7 +69,7 @@ app.get("/logout", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(err => {
@@ -83,7 +85,7 @@ app.post("/register", (req, res) => {
 app.post("/login", (req, res) => {
 
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
@@ -94,12 +96,12 @@ app.post("/login", (req, res) => {
           res.render("secrets");
         }
       }
-    }
+    } 
   });
 });
 
 
 // Port
 app.listen(process.env.PORT, () => {
-  console.log("Server running on port 3000...✅");
+  console.log(`Server running on port ${process.env.PORT}...✅`);
 });
